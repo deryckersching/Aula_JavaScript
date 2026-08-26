a29f5b81811b45757c4c8daa10d96fa7da299727
deryckersching <deryckersching@gmail.com>
2026-08-25 21:41:16 -0300

    Update script.js

--- aula_06_js/frontend/scripts/script.js
+++ aula_06_js/frontend/scripts/script.js
@@ -1,79 +1,17 @@
-console.log("Olá, Mundo!")
+console.log("Olá, Mundoooo!")
 
-// VARIÁVEIS
+// EXERCÍCIOS - DERYCK ERSCHING ZEZUINO
 
-const userName = "Ana";
-let usersList = ["Ana", "Bento", "Carlos", "Diana"];
+// QUESTÃO 1. Selecione o <h1> usando querySelector e 
+// mostre o elemento inteiro no console, para confirmar 
+// que pegou o elemento certo.
 
-// FUNÇÕES
+const titulo = document.querySelector("h1");
+console.log(titulo);
 
-// Cria a função
-function logName(name) {
-    console.log(name);
-};
+// QUESTÃO 2. Selecione o parágrafo de classe subtitulo e mostre 
+// no console só o texto dele (não o elemento inteiro).
 
-// "Chama" a função --> é quando ela é executada
-logName(userName);
-
-// SELETORES DE ELEMENTOS
-
-// <p> --> tag de abertura
-// </p> --> tag de fechamento
-// Olá, Mundo! --> conteúdo
-// <p>Olá, Mundo!</p> --> elemento
-
-// Método get:
-
-// getElementById() --> Seleciona UM elemento pelo id
-// const titulo = document.getElementById("titulo");
-// console.log(titulo);
-
-// getElementsByClassName() --> Seleciona elementos pela classe
-// const textos = document.getElementsByClassName("texto");
-// console.log(textos);
-// console.log(textos[0]);
-
-// getElementsByTagName() --> Seleciona elementos pela tag HTML
-const paragrafos = document.getElementsByTagName("p");
-console.log(paragrafos);
-console.log(paragrafos[1]);
-
-// Métodos query:
-
-// querySelector() --> Seleciona o primeiro elemnto que corresponde ao seletor CSS
-
-// ID
-// const titulo = document.querySelector("#titulo");
-// console.log(titulo);
-
-// Class
-const texto = document.querySelector(".texto");
-console.log(texto);
-
-// Tag HTML
-const paragrafo = document.querySelector("p");
-console.log(paragrafo)
-
-// Elementro dentro de elemento
-const tituloMain = document.querySelector("main h1")
-console.log(tituloMain)
-
-// querySelectorAll() --> Seleciona todos os elementos correspondente ao seletor CSS
 const textos = document.querySelectorAll(".texto");
 console.log(textos);
 console.log(textos[2]);
-
-// MODIFICADORES DE CONTEÚDO
-// Formas de ler e alterar conteúdos de um elemento HTML
-
-const titulo = document.getElementById("titulo");
-
-// textContent --> pega ou altera o TEXTO que está dentro do elemento, independente se estiver visível ou não
-console.log(titulo.textContent);
-// titulo.textContent = "<strong>Teste</strong>";
-
-// innerText --> igual ao textContent, mas, só pega o que está visível na tela
-// console.log(titulo.innerText);
-
-// innerHTML --> trabalha com HTML
-titulo.innerHTML += "<strong>Teste</strong>";
